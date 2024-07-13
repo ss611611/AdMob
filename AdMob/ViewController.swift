c724d142d329f24ef381246b921141db976bd5c5
jackie <ss611611@gmail.com>
2024-07-13 16:48:37 +0800

    Initial commit

---
+++ AdMob/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AdMob
+//
+//  Created by Jackie Lu on 2024/7/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
